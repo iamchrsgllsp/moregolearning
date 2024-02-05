fb969c7ff0df26547303d9d174ac6cde4d2f3a03
iamchrgllsp <chrisgillespie2001@googlemail.com>
2024-02-05 14:28:04 +0000

    small testing for db

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"main/anotherpkg"
 	"main/app"
 	"main/dbFunc"
+	"time"
 )
 
 func Workflow() {
@@ -15,16 +16,27 @@ func Workflow() {
 	app.Discord()
 	// Using var based on the maths Addtion function
 	var data2 = app.Addition(400, 2)
-	dbFunc.CreateDB()
-  dbFunc.AddtoDB("Chris")
-  dbFunc.ReadDB()
 	anotherpkg.DryRun()
 	// fmt.Println prints to terminal
 	fmt.Println(data)
 	fmt.Println(data2)
 }
 
+func DbActions() {
+	dbFunc.CreateDB()
+	dbFunc.AddtoDB("Raidy")
+	dbFunc.AddtoDB("Chris")
+  dbFunc.AddtoDB("Goose")
+  dbFunc.AddtoDB("Bun")
+  dbFunc.AddtoDB("Dave")
+  time.Sleep(10)
+	dbFunc.ReadDB()
+}
+
 func main() {
-	// main is entry point to programme - then we run workflow
+	// main is entry point to programme - then we run our actions
 	Workflow()
+  DbActions()
+	
+
 }
