da7d94065558a4bca4b84004966c9761b1f2cbc9
iamchrsgllsp <chrisgillespie2001@googlemail.com>
2024-02-05 13:22:26 +0000

    simple func to create sqlite3 db

--- dbFunc/db.go
+++ dbFunc/db.go
@@ -5,17 +5,35 @@ package dbFunc
 import (
 	"database/sql"
 	"fmt"
-	"github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
-	"path/filepath"
+  
 )
+
+//set filename
+const fileName = "/home/runner/moregolearning/dbFunc/foo.db"
 
 func CreateDB() {
 	fmt.Println("Creating DB")
-	fileName := "test.db"
+
+  // open database/sql using sqlite3 as driver and fileName as data source
+  // return db object and err
+  // if err is not nil, then log the error
 	db, err := sql.Open("sqlite3", fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
-  fmt.Println(db)
+  // defer close until actions have been run
+	defer db.Close()
+
+  //prep simple sql query to create table
+	sqlStmt := `
+  create table foo (id integer not null primary key, name text);
+  delete from foo;
+  `
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
 }
