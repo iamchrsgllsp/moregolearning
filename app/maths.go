7607b506d014a7074e75a9679e78df6ecd0e8d4c
iamchrsgllsp <chrisgillespie2001@googlemail.com>
2024-02-05 10:50:59 +0000

    add maths learning

---
+++ app/maths.go
@@ -0,0 +1,6 @@
+package app
+
+func Addition(n1 int, n2 int) int {
+  val := n1 + n2
+  return val
+}
--- main.go
+++ main.go
@@ -5,8 +5,15 @@ import (
 	"main/app"
 )
 
-func main() {
+func Workflow() {
+	//Quick check on imports
 	data := app.PassingVars()
 	app.Discord()
+  var data2 = app.Addition(400,2)
 	fmt.Println(data)
+  fmt.Println(data2)
+}
+
+func main() {
+	Workflow()
 }
