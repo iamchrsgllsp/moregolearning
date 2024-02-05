6de389fceccd5554d02cdd90e926674402430b0d
iamchrsgllsp <chrisgillespie2001@googlemail.com>
2024-02-05 10:57:06 +0000

    improved comments

--- main.go
+++ main.go
@@ -7,13 +7,19 @@ import (
 
 func Workflow() {
 	//Quick check on imports
+	// := is a type assertion based on the type of the variable
 	data := app.PassingVars()
+  // Running the discord.go function Discord
 	app.Discord()
-  var data2 = app.Addition(400,2)
+	// Using var based on the maths Addtion function
+	var data2 = app.Addition(400, 2)
+
+  // fmt.Println prints to terminal
 	fmt.Println(data)
-  fmt.Println(data2)
+	fmt.Println(data2)
 }
 
 func main() {
+  // main is entry point to programme - then we run workflow
 	Workflow()
 }
--- readme.md
+++ readme.md
@@ -1,9 +1,11 @@
 Repo to hold all GO Learning
+More thorough learning on Notion: LINK
 
 - Setting and using variables
 - Importing packages
 - folder structure
 - simple discord bot
-- 
+- learn concurrency and channels: make(chan ...)
+  - difference between buffered and unbuffered channels
 
 
